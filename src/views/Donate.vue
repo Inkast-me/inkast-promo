6beea2221ed8946b4ae8bd097fd90c5a039e50ee
SeamMiner <niatit130@gmail.com>
2022-04-05 21:08:02 +0300

    Add donate page

---
+++ src/views/Donate.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    console.log('This is donate page');
+        
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
